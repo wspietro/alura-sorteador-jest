b529715ee5b72735c8aeaf557263701145e885bf
Pietro Weg <pietro.sera@compasso.com.br>
2022-10-12 15:07:43 -0300

    :sparkles: feat: manipulando estado de erro

--- src/state/hooks/useAddParticipante.ts
+++ src/state/hooks/useAddParticipante.ts
@@ -1,9 +1,16 @@
-import { useSetRecoilState } from "recoil";
-import { listaParticipantesState } from "../atom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { erroState, listaParticipantesState } from "../atom";
 
 export const useAddParticipante = () => {
   const setLista = useSetRecoilState(listaParticipantesState);
+  const lista = useRecoilValue(listaParticipantesState);
+  const setErro = useSetRecoilState(erroState);
+
   return (nomeDoParticipante: string) => {
+    if (lista.includes(nomeDoParticipante)) {
+      setErro('Nomes duplicados não são permitidos');
+      return
+    }
     return setLista(listaAtual => [...listaAtual, nomeDoParticipante])
   };
 };
